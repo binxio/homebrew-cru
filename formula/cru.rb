51467e3eb5a4b3c6b493afea19cc6942307075ed
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2023-08-08 22:25:24 +0200

    Brew formula update for cru version 2.0.2

--- formula/cru.rb
+++ formula/cru.rb
@@ -10,7 +10,7 @@ class Cru < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/binxio/cru/releases/download/2.0.2/cru_2.0.2_darwin_arm64.tar.gz"
-      sha256 "fd23b001869632d2324f1d455e7d864476e6e6a378d07d1b2ff056b0559253c2"
+      sha256 "66cd22e70e06e1c77072465cb42d31f290f06a588a4c3d7ef7681d37717853c5"
 
       def install
         bin.install "cru"
@@ -18,7 +18,7 @@ class Cru < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/binxio/cru/releases/download/2.0.2/cru_2.0.2_darwin_amd64.tar.gz"
-      sha256 "457fc3d293b4d1bc7c5cf7bfa683b2790c4e8413103f796b98a51c934c58a51d"
+      sha256 "a9db4654f70166fa03699b0486fde745b046b4f712ab03d8eded71bc71a85120"
 
       def install
         bin.install "cru"
@@ -29,7 +29,7 @@ class Cru < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/binxio/cru/releases/download/2.0.2/cru_2.0.2_linux_arm64.tar.gz"
-      sha256 "779e19239f4add7d19c1a15280c4da85abf03c59cfbe09038e3f90c31d9d5b35"
+      sha256 "16818ccf363526379105d0d2bafcd4444f68695683ec76d63ca138e8d594a5f0"
 
       def install
         bin.install "cru"
@@ -37,11 +37,15 @@ class Cru < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/binxio/cru/releases/download/2.0.2/cru_2.0.2_linux_amd64.tar.gz"
-      sha256 "a810753e8b0f2e4c10892c132c0cb1b21d8198b88eb122a3a2fedc0c0bd15585"
+      sha256 "c15d56cf68f89917879fbea57e0a21a74f4553d61ffc7c32b21be44ddfe49925"
 
       def install
         bin.install "cru"
       end
     end
+  end
+
+  test do
+    system "#{bin}/cru --help"
   end
 end
